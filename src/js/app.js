771ee47862625f00320c2eea91638718d0140fed
Stark <serj.elessar@gmail.com>
2022-09-16 20:40:14 +0300

    init: update template

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
 import * as flsFunctions from './modules/functions.js'
 
 flsFunctions.isWebp()
-flsFunctions.windowPopup()
-
-import Swiper, { Navigation, Pagination } from 'swiper'
-
-const swiper = new Swiper()
